c475c2d85ead951b2e76f9f54f378881eb89521b
luhackk <luhack@gmail.com>
2026-09-14 12:23:47 +0000

    feat: add enums library

---
+++ src/main/java/project/lybrary/domain/library/enums/BookCategory.java
@@ -0,0 +1,18 @@
+package project.lybrary.domain.library.enums;
+
+public enum BookCategory {
+
+    FICTION,
+    ROMANCE,
+    FANTASY,
+    SCI_FI,
+    MYSTERY,
+    THRILLER,
+    HISTORICAL,
+    HORROR,
+    LITERARY,
+    ADVENTURE,
+    WESTERN,
+    GRAPHIC_NOVEL,
+    
+}
---
+++ src/main/java/project/lybrary/domain/library/enums/LoanStatus.java
@@ -0,0 +1,9 @@
+package project.lybrary.domain.library.enums;
+
+public enum LoanStatus {
+    
+    ACTIVE,
+    RETURNED,
+    DELAYED,
+    
+}
